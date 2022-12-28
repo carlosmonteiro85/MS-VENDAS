b431c8f2a07b043b37f8d24da2056f2c92527ec4
carlosmonteiro85 <carlosmonteiro1985@outlook.com>
2022-12-27 23:06:23 -0300

    Configuração do config server

--- ms-produto/src/main/java/com/prototype/api/controller/ProdutoController.java
+++ ms-produto/src/main/java/com/prototype/api/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package com.prototype.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.prototype.api.dto.ProdutoRequest;
 import com.prototype.api.dto.ProdutoResponse;
+import com.prototype.core.modelmapper.Configuration;
 import com.prototype.domain.service.ProdutoService;
 import com.prototype.domain.utils.AppConstants;
 
@@ -41,5 +43,13 @@ public class ProdutoController {
 	public ResponseEntity<ProdutoResponse> findProdutoCodigo(@RequestParam("codigo") String codigo) {
 		return ResponseEntity.ok().body(service.findByCodigo(codigo));
 	}
+	
+	 @Autowired
+	    private Configuration configuration;
+
+	    @GetMapping("/endpoint")
+	    public String retrieveLimits(){
+	        return configuration.getValue();
+	    }
 
 }
---
+++ ms-produto/src/main/resources/db/testdata/afterMigrate.sql
@@ -0,0 +1,11 @@
+INSERT INTO produtos
+(codigo_barras, descricao, nome, preco)
+VALUES('123456789', 'Noot book Gamer com 16GB de ddr4, m2 de 500GB ', 'Nitro 5', 4000.0);
+
+INSERT INTO produtos
+(codigo_barras, descricao, nome, preco)
+VALUES('789456123', 'Celular da Samsung', 'Galaxy s20 FE', 2000.0);
+
+INSERT INTO produtos
+(codigo_barras, descricao, nome, preco)
+VALUES('456789123', 'monitor de 29 p', 'Monitor LG', 1400.0);
